7794a093f273b96c1760b025d19c93d8bd5844dd
Simone <info@simoneicardi.com>
2025-10-30 11:47:47 +0100

    controller index books

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,7 +3,18 @@ const connection = require('../data/db');
 
 //  INDEX
 function index(req, res) {
-    console.log("hai richiesto la rotta index");
+
+    // impostiamo la query
+    const sql = 'SELECT * FROM books';
+
+    // aggiungiamo la connesione per la richiesta
+    connection.query(sql, (err, result) => {
+        // gestiamo errore server mysql
+        if (err) return res.status(500).json({ error: "Database error" })
+        // ritorniamo il risultato ottenuto
+        res.json(result);
+    });
+
 }
 
 //  SHOW
